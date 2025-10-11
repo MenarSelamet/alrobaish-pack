44cc688e3e6f09eb79bdf2f8753dbcdc52d8d2ae
Menar Selamet <menarselamet007@gmail.com>
2025-10-11 20:55:19 +0300

    Contact Page Hero Section

--- resources/js/Pages/Contact.jsx
+++ resources/js/Pages/Contact.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import GuestLayout from "../Layouts/GuestLayout";
 
 const Contact = () => {
-  return (
-    <div>
-      Contact Page
-    </div>
-  )
-}
+    return (
+        <GuestLayout>
+            <div className="min-h-screen">
+                {/* Hero Section */}
+                <section className="py-20 bg-gradient-to-b from-primary/5 to-background">
+                    <div className="container mx-auto px-4">
+                        <div className="max-w-3xl mx-auto text-center">
+                            <h1 className="text-5xl font-bold text-foreground mb-6">
+                                Contact Us
+                            </h1>
+                            <p className="text-xl text-muted-foreground">
+                                Have questions? We'd love to hear from you. Send
+                                us a message and we'll respond as soon as
+                                possible.
+                            </p>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </GuestLayout>
+    );
+};
 
-export default Contact
+export default Contact;
--- resources/js/Pages/Products.jsx
+++ resources/js/Pages/Products.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import GuestLayout from '../Layouts/GuestLayout'
 
 const Products = () => {
   return (
-    <div>
+    <GuestLayout>
       Product Page
-    </div>
+    </GuestLayout>
   )
 }
 
