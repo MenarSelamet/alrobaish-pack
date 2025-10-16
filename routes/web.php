ed552d41d6c731a3e293e736df433d8bdcdaaa30
Menar Selamet <menarselamet007@gmail.com>
2025-10-16 15:58:17 +0300

    Changing dashboard to allow admins only

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+Route::get('admin/dashboard', function () {
+    return Inertia::render('Admin/Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
